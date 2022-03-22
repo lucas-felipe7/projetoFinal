d372f2d8e7caba861ddf0462c3ce3c767a1f013b
yan Olv <yanhenriquefree@gmail.com>
2022-03-21 22:24:46 -0300

    Navigator

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import Home from './src/screens/Menu';
+import Home from './src/screens/Home';
 import Shape from './src/screens/Shapes';
 import Trucks from './src/screens/Trucks';
 import Rodas from './src/screens/Rodas';
