91e7acd464a062228691a483464c2012b39ac546
yan Olv <yanhenriquefree@gmail.com>
2022-03-21 22:31:29 -0300

    Navigator

--- App.js
+++ App.js
@@ -3,11 +3,11 @@ import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import Home from './src/screens/Home';
-import Shape from './src/screens/Shapes';
-import Trucks from './src/screens/Trucks';
-import Rodas from './src/screens/Rodas';
-import Login from './src/screens/Login';
+import Home from './src/screens/Home/Menu';
+import Shape from './src/screens/Shapes/Shapes';
+import Trucks from './src/screens/Trucks/Trucks';
+import Rodas from './src/screens/Rodas/Wheels';
+import Login from './src/screens/Login/Login';
 
 
 
@@ -17,7 +17,7 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName={Home}>
-        <Stack.Screen name="Home" component={Home} options={{ title: 'Maze',}}/>
+        <Stack.Screen name="Home" component={Home} options={{ title: 'Maze' }}/>
         <Stack.Screen name="Shape" component={Shape} />
         <Stack.Screen name="Trucks" component={Trucks} />
         <Stack.Screen name="Rodas" component={Rodas} />
