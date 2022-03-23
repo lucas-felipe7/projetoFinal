33848511b5b62b0a921185d06344e7299276f9c2
lucas-felipe7 <87493055+lucas-felipe7@users.noreply.github.com>
2022-03-22 21:52:52 -0300

    styling menu and shapes

--- src/screens/Home/Menu.js
+++ src/screens/Home/Menu.js
@@ -1,57 +1,54 @@
 import React from 'react';
-import { View, Text, Button, StatusBar, Image, StyleSheet  } from 'react-native';
+import { View, Text, Button, StatusBar, Image, StyleSheet } from 'react-native';
 
-export default function Home({navigation}) {
- 
+export default function Home({ navigation }) {
+
   return (
-   <View style={{flexDirection: "column", backgroundColor:"#fff" }}>
-      <StatusBar barStyle="dar-content" hidden = {false} backgroundColor="black"/>
-      <View style={{flexDirection:"row", backgroundColor: "black"}}>
-      <Button
-        
-        title =  "  Shapes  "
-        color="black"
-        onPress = { () =>
-          navigation.navigate('Shape')}
-          />
+    <View style={{ flexDirection: "column", backgroundColor: '#fff' }}>
+      <StatusBar barStyle="dark-content" hidden={true} backgroundColor="black" />
+      <View style={{ flexDirection: "row", backgroundColor: "black", justifyContent: 'space-around' }}>
+
         <Button
-        
-        title = " Trucks  "
-        color="black"
-        onPress = { () =>
-          navigation.navigate('Trucks')}
+          title="Shapes"
+          color="black"
+          onPress={() =>
+            navigation.navigate('Shape')}
         />
-      <Button
-        title = "  Rodas  "
-        color="black"
-        onPress = { () =>
-          navigation.navigate('Rodas')}
-         />
-         <Button
-        title = "  Login  "
-        color="black"
-        onPress = { () =>
-          navigation.navigate('Login')}
-         />
-         
-      </View>
-      
-      <View style={{paddingTop:40, paddingLeft:2}}>
-      <Image source={require('../../media/assets/images/m57.png')} style={{ width: 350, height: 100,}}/>
 
-      <Text style={{color:"black", fontSize:20, paddingLeft:110,}}>SKATE SHOP</Text>
-      </View>
-      
-      <View style={{paddingTop:211, paddingLeft:2, }}>
-      <Image source={require('../../media/assets/images/ramp.webp')} style={{ width: 350, height: 100,}}/>
+        <Button
+          title="Trucks"
+          color="black"
+          onPress={() =>
+            navigation.navigate('Trucks')}
+        />
 
-      
+        <Button
+          title="Rodas"
+          color="black"
+          onPress={() =>
+            navigation.navigate('Rodas')}
+        />
+
+        <Button
+          title="Login"
+          color="black"
+          onPress={() =>
+            navigation.navigate('Login')}
+        />
+
       </View>
-      <View style={{flexDirection:"column", backgroundColor:"black", paddingTop:15}}>
-        <Text style={{color:"#fff", paddingLeft:110, }}>Maze skate shop</Text>
+
+      <View style={{ paddingTop: '35%' }}>
+        <Image source={require('../../media/assets/images/m57.png')} style={{ width: '100%', height: 150, }} />
+        <Text style={{ color: "black", fontSize: 20, textAlign: 'center', fontWeight: '600' }}>SKATE SHOP</Text>
       </View>
-   </View>  
- );
+
+      <View style={{ paddingTop: '10%' }}>
+        <Image source={require('../../media/assets/images/ramp.webp')} style={{ width: '100%', height: 150, }} />
+      </View>
+
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
--- src/screens/Shapes/Shapes.js
+++ src/screens/Shapes/Shapes.js
@@ -1,117 +1,114 @@
 import React from 'react';
-import { View, Text, Button, StatusBar, Image, StyleSheet, SafeAreaView, ScrollView, Alert  } from 'react-native';
+import { View, Text, Button, StatusBar, Image, StyleSheet, SafeAreaView, ScrollView, Alert } from 'react-native';
 
-export default function Shape({navigation}) {
-    const Separator = () => (
-        <View style={styles.separator} />
-      );
-    return(
-        <View style={{flexDirection: "column", backgroundColor:"#fff" }}>
-        <StatusBar barStyle="dar-content" hidden = {false} backgroundColor="black"/>
-        
-        <SafeAreaView>
+export default function Shape({ navigation }) {
+  const Separator = () => (
+    <View style={styles.separator} />
+  );
+  return (
+    <View style={{ flexDirection: "column", backgroundColor: "#fff" }}>
+      <StatusBar barStyle="dark-content" hidden={true} backgroundColor="black" />
+
+      <SafeAreaView>
         <ScrollView style={[styles.scrollView,]}>
-          
-          <View style={{flexDirection:"column", backgroundColor:'#fff'}}>
-            <Image source={require('../../media/assets/images/shap.png')} style={{ width: 330, height: 100,}}/>
-  
-            <Text style={{paddingLeft:30, fontSize:25, color:'black'}}>Modelos disponiveis</Text>
+
+          <View style={{ flexDirection: "column", backgroundColor: '#fff' }}>
+
+            <Image source={require('../../media/assets/images/shap.png')} style={{ width: '100%', height: 100, }} />
             <Separator />
-  
-            <View style={{flexDirection:'column', paddingTop:42, paddingLeft:32, paddingRight:52 }}>
-            <Image source={require('../../media/assets/images/dgk.jpg')} style={{ width: 250, height: 300, paddingTop:22}}/>
-            <Text style={{paddingLeft:30, fontSize:12, color:'black'}}>shape dgk tamanho 8.0</Text>
-            <Text style={{paddingLeft:10, fontSize:12, color:'black',paddingTop:13,padding:7}}>R$ 350,00</Text>
-            <Button title="Comprar" color="black"
-             onPress={() => Alert.alert('Fora do estoque')}
-            />  
+
+            <View style={{ flexDirection: 'column', paddingTop: "10%", justifyContent: "center", alignItems: "center" }}>
+              <Image source={require('../../media/assets/images/dgk.jpg')} style={{ width: 300, height: 300, paddingTop: 22 }} />
+              <Text style={{ fontSize: 16, color: 'black', fontWeight: "600" }}>SHAPE DGK TAMANHO 8.0</Text>
+              <Text style={{ fontSize: 16, color: 'black', paddingTop: 10, fontWeight: 'bold' }}>R$ 350,00</Text>
+              <Button title="Comprar" color="black"
+                onPress={() => Alert.alert('OUT OF STOCK')}
+              />
             </View>
-  
+
             <Separator />
-  
-            <View style={{flexDirection:'column', paddingTop:42, paddingLeft:32, paddingRight:52 }}>
-            <Image source={require('../../media/assets/images/creature.webp')} style={{ width: 250, height: 300,}}/>
-            <Text style={{paddingLeft:10, fontSize:12, color:'black', paddingTop:15}}>shape creature tamanho 8.5 </Text>  
-            <Text style={{paddingLeft:10, fontSize:12, color:'black',paddingTop:13,padding:7}}>R$ 390,00</Text>
-            <Button title="Comprar" color="black"
-             onPress={() => Alert.alert('Fora do estoque')}
-            />      
+
+            <View style={{ flexDirection: 'column', paddingTop: "10%", justifyContent: "center", alignItems: "center" }}>
+              <Image source={require('../../media/assets/images/creature.webp')} style={{ width: 250, height: 300, }} />
+              <Text style={{ fontSize: 16, color: 'black', fontWeight: "600" }}>SHAPE CREATURE TAMANHO 8.5 </Text>
+              <Text style={{ fontSize: 16, color: 'black', paddingTop: 10, fontWeight: 'bold' }}>R$ 390,00</Text>
+              <Button title="Comprar" color="black"
+                onPress={() => Alert.alert('OUT OF STOCK')}
+              />
             </View>
-  
+
             <Separator />
-  
-            <View style={{flexDirection:'column', paddingTop:42, paddingLeft:32, paddingRight:52}}>
-            <Image source={require('../../media/assets/images/flip.jpg')} style={{ width: 250, height: 300,}}/>
-            <Text style={{paddingLeft:30, fontSize:12, color:'black'}}>shape flip tamanho 8.1</Text>  
-            <Text style={{paddingLeft:10, fontSize:12, color:'black',}}>R$ 490,00</Text>
-            <Button title="Comprar" color="black"
-             onPress={() => Alert.alert('Fora do estoque')}
-            />          
-            </View>
-            <Separator />
-  
-            <View style={{flexDirection:'column', paddingTop:42, paddingLeft:32, paddingRight:52 }}>
-            <Image source={require('../../media/assets/images/plan.jpg')} style={{ width: 150, height: 300,}}/>
-            <Text style={{paddingLeft:30, fontSize:12, color:'black'}}>shape  planb tamanho 7.8</Text> 
-            <Text style={{paddingLeft:10, fontSize:12, color:'black',paddingTop:13,padding:7}}>R$ 550,00</Text>
-            <Button title="Comprar" color="black"
-             onPress={() => Alert.alert('Fora do estoque')}
-            />            
+
+            <View style={{ flexDirection: 'column', paddingTop: "10%", justifyContent: "center", alignItems: "center" }}>
+              <Image source={require('../../media/assets/images/flip.jpg')} style={{ width: 250, height: 300, }} />
+              <Text style={{ fontSize: 16, color: 'black', fontWeight: "600" }}>SHAPE FLIP TAMANHO 8.1</Text>
+              <Text style={{ fontSize: 16, color: 'black', paddingTop: 10, fontWeight: 'bold' }}>R$ 490,00</Text>
+              <Button title="Comprar" color="black"
+                onPress={() => Alert.alert('OUT OF STOCK')}
+              />
             </View>
             <Separator />
-  
-  
-            <View style={{flexDirection:'column', paddingTop:42, paddingLeft:32 }}>
-            <Image source={require('../../media/assets/images/girl.jpg')} style={{ width: 240, height: 320,}}/>
-            <Text style={{paddingLeft:30, fontSize:12, color:'black'}}>shape toy machine tamanho 8.2</Text>
-            <Text style={{paddingLeft:10, fontSize:12, color:'black',paddingTop:13,padding:7}}>R$ 400,00</Text>
-            <Button title="Comprar" color="black"
-             onPress={() => Alert.alert('Fora do estoque')}
-            />  
+
+            <View style={{ flexDirection: 'column', paddingTop: "10%", justifyContent: "center", alignItems: "center" }}>
+              <Image source={require('../../media/assets/images/plan.jpg')} style={{ width: 150, height: 300, }} />
+              <Text style={{ fontSize: 16, color: 'black', fontWeight: "600" }}>SHAPE PLANB TAMANHO 7.8</Text>
+              <Text style={{ fontSize: 16, color: 'black', paddingTop: 10, fontWeight: 'bold' }}>R$ 550,00</Text>
+              <Button title="Comprar" color="black"
+                onPress={() => Alert.alert('OUT OF STOCK')}
+              />
             </View>
             <Separator />
-  
-            <View style={{flexDirection:'column', paddingTop:42, paddingLeft:32, paddingRight:52 }}>
-            <Image source={require('../../media/assets/images/pri.jpg')} style={{ width: 200, height: 350,}}/>
-            <Text style={{ fontSize:12, color:'black'}}>shape primitive tamanho 8.25</Text>
-            <Text style={{paddingLeft:10, fontSize:12, color:'black',paddingTop:13,padding:7}}>R$ 550,00</Text>
-            <Button title="Comprar" color="black"
-             onPress={() => Alert.alert('Fora do estoque')}
-            />  
+
+
+            <View style={{ flexDirection: 'column', paddingTop: "10%", justifyContent: "center", alignItems: "center" }}>
+              <Image source={require('../../media/assets/images/girl.jpg')} style={{ width: 240, height: 320 }} />
+              <Text style={{ fontSize: 16, color: 'black', fontWeight: "600" }}>SHAPE TOY MACHINE TAMANHO 8.2</Text>
+              <Text style={{ fontSize: 16, color: 'black', paddingTop: 10, fontWeight: 'bold' }}>R$ 400,00</Text>
+              <Button title="Comprar" color="black"
+                onPress={() => Alert.alert('OUT OF STOCK')}
+              />
             </View>
             <Separator />
-  
-            
-  
-            <View style={{lexDirection: "column", backgroundColor:"#fff"}}>
-                <Text style={{fontSize:13, color:'black', padding:4}}>7.5 a 8, para stret, 8.0 a 8.25 é para “Vert ou mini ramp”, "8.25” a "8.5"é para Bowls e rampas verticais (Half-pipes e mini-ramps) </Text>
-                <Text style={{fontSize:13, color:'black', padding:4}}>Vai de acordo com o gosto e tamanho do pé</Text>
-                <Text style={{fontSize:13, color:'black', padding:4}}>"8.25” a "8.5"é para Bowls e rampas verticais (Half-pipes e mini-ramps) 
-                </Text>
+
+            <View style={{ flexDirection: 'column', paddingTop: "10%", justifyContent: "center", alignItems: "center" }}>
+              <Image source={require('../../media/assets/images/pri.jpg')} style={{ width: 240, height: 320, }} />
+              <Text style={{ fontSize: 16, color: 'black', fontWeight: "600" }}>SHAPE PRIMITIVE TAMANHO 8.25</Text>
+              <Text style={{ fontSize: 16, color: 'black', paddingTop: 10, fontWeight: 'bold' }}>R$ 550,00</Text>
+              <Button title="Comprar" color="black"
+                onPress={() => Alert.alert('OUT OF STOCK')}
+              />
             </View>
-            <View style={{flexDirection:"column", backgroundColor:"black", }}>
-               <Text style={{color:"#fff", paddingLeft:110, }}>Maze skate shop</Text>
+            <Separator />
+
+
+
+            <View style={{ lexDirection: "column", backgroundColor: "#fff", marginTop: "2%", marginBottom: "5%" }}>
+              <Text style={{ textAlign: "center", color: "black", fontSize: 16, fontWeight: 'bold' }}>Sobre os tamanhos dos shapes</Text>
+              <Text style={{ fontSize: 16, color: 'black', padding: 8, textAlign: "center" }}>Do tamanho 7.5 ao 8 para a modalidade Street, do tamanho 8.0 ao 8.25 é para a modalidade “Vert" ou "Mini Ramp”, e do tamanho 8.25 ao 8.5 é para Bowls e rampas verticais (Half-pipes) </Text>
+              <Text style={{ fontSize: 16, color: 'black', padding: 8, textAlign: "center" }}>Também muito importante, o tamanho do shape varia de acordo com o gosto pessoal e o tamanho do seu pé</Text>
             </View>
-            
+            <View style={{ flexDirection: "column", backgroundColor: "black", }}>
+              <Text style={{ color: "#fff", textAlign: "center" }}>POR LUCAS FELIPE E YAN OLIVEIRA</Text>
+            </View>
+
           </View>
         </ScrollView>
-        </SafeAreaView>
-        
-     </View>  
-    );
-  }
+      </SafeAreaView>
 
-  const styles = StyleSheet.create({
-  
-  
+    </View>
+  );
+}
 
-    scrollView: {
-      backgroundColor: '#fff',
-      marginHorizontal: 15,
-    },
-    separator: {
-      marginVertical: 8,
-      borderBottomColor: '#737373',
-      borderBottomWidth: StyleSheet.hairlineWidth,
-    },
-  });
+const styles = StyleSheet.create({
+
+
+
+  scrollView: {
+    backgroundColor: '#fff',
+  },
+  separator: {
+    marginVertical: 15,
+    borderBottomColor: '#737373',
+    borderBottomWidth: 2.5
+  },
+});
